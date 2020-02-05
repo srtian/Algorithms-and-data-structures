43c291afcec1cc006b1a965ea8675ddfc1ba23c3
ruotian.shen <shenruotian@163.com>
2020-02-05 17:01:44 +0800

    add: add the bubbleSort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -5,13 +5,16 @@
 function bubbleSort(arr) {
   const len = arr.length;
   for (let i = 0; i < len - 1; i++) {
+    let flag = false;
     for (let j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         const val = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = val;
+        flag = true;
       }
     }
+    if (flag) break;
   }
   return arr;
 }
