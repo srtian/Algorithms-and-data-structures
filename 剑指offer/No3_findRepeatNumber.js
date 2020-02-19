7369653aa143db94a16db8db57f6b0b83148ff3d
ruotian.shen <shenruotian@163.com>
2020-02-19 21:49:08 +0800

    add: jianzhiofferno3 sort solution

--- 剑指offer/No3_findRepeatNumber.js
+++ 剑指offer/No3_findRepeatNumber.js
@@ -12,3 +12,18 @@ var findRepeatNumber = function(nums) {
     obj[nums[i]] = i;
   }
 };
+
+// sort solution
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findRepeatNumber = function(nums) {
+  const len = nums.length;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < len - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return nums[i];
+    }
+  }
+};
