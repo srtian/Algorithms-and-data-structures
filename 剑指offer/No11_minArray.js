b796c2c20d84b2b45b1be107df601f297068b333
ruotian.shen <ruotian.shen@horizon.ai>
2020-02-23 22:49:04 +0800

    add: leetcode no11 minArray

---
+++ 剑指offer/No11_minArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var minArray = function(numbers) {
+  let j = 0;
+  let i = numbers.length - 1;
+  while (j < i) {
+    let mid = Math.floor((i + j) / 2);
+    if (numbers[mid] > numbers[i]) {
+      j = mid + 1;
+    } else if (numbers[mid] < numbers[i]) {
+      i = mid;
+    } else {
+      i--;
+    }
+  }
+  return numbers[i];
+};
