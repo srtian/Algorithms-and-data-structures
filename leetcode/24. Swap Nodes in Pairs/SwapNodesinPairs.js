c1f3ee9764e524afc7ae82fae6ee5b781699ff2a
ruotian.shen <ruotian.shen@horizon.ai>
2020-02-26 21:04:17 +0800

    add: slove the no.12 swap node in pairs

--- leetcode/24. Swap Nodes in Pairs/SwapNodesinPairs.js
+++ leetcode/24. Swap Nodes in Pairs/SwapNodesinPairs.js
@@ -9,16 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
+var swapPairs = function(head) {
+  if (!head || !head.next) return head;
   let arr = [];
   while (head) {
     arr.push(head.val);
     head = head.next;
   }
-  let len = 0;
-  arr.reverse();
+  for (let i = 0; i < arr.length - 1; i = i + 2) {
+    let flag = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = flag;
+  }
   let node = new ListNode();
   let list = node;
+  let len = 0;
   while (len < arr.length) {
     node.next = {
       val: arr[len],
