e2e10668086e6382c1367fee728efdfcdad5fc55
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-05 10:31:19 +0800

    add: slove the no.122 the best time to sell stock ||

---
+++ leetcode/122.the_best_time_to_sell_stock/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let profit = 0;
+  let size = prices.length;
+  for (let i = 1; i < size; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit = profit + prices[i] - prices[i - 1];
+    }
+  }
+  return profit;
+};
