6045ad1e867063213e7ad6d4fd79e40d8d410ca7
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-06 16:03:44 +0800

    add: slove the no 350 inntersectionn of two arrays

---
+++ leetcode/350.Intersection_of_Two_Arrays_II/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  let dict = {};
+  let size1 = nums1.length;
+  let size2 = nums2.length;
+  let arr = [];
+  for (let i = 0; i < size1; i++) {
+    if (!dict[nums1[i]]) {
+      dict[nums1[i]] = 1;
+    } else {
+      dict[nums1[i]] = dict[nums1[i]] + 1;
+    }
+  }
+  for (let j = 0; j < size2; j++) {
+    if (dict[nums2[j]] > 0) {
+      arr.push(nums2[j]);
+      dict[nums2[j]] = dict[nums2[j]] - 1;
+    }
+  }
+  return arr;
+};
