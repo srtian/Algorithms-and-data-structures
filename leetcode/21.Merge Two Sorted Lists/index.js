2361d97675b7c9cf9fe235920b3cbf759af284f4
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-11 21:26:11 +0800

    add the leetcode no21 merge two sorted lists

---
+++ leetcode/21.Merge Two Sorted Lists/index.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function mergeTwoList(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val > l2.val) {
+    l2.next = mergeTwoList(l2.next, l1);
+    return l2;
+  } else {
+    l1.next = mergeTwoList(l1.next, l2);
+    return l1;
+  }
+  return l1;
+}
