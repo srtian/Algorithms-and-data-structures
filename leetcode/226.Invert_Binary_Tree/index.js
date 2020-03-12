d8a24452a7ce7df2b410fcb22e2a570a199ab32f
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-12 22:31:31 +0800

    add the method3 for no226 invert binary tree

--- leetcode/226.Invert_Binary_Tree/index.js
+++ leetcode/226.Invert_Binary_Tree/index.js
@@ -33,3 +33,13 @@ var invertTree = function(root) {
   root.left = right;
   return root;
 };
+
+var invertTree = function(root) {
+  if (!root) return root;
+  const left = root.left;
+  root.left = root.right;
+  root.right = left;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
