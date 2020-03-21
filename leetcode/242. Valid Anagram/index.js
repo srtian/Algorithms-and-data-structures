52007bdf3071b4bca6dfda30c0914166f4b18ac2
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-21 17:23:17 +0800

    add no242 vaild anagram the second method

--- leetcode/242. Valid Anagram/index.js
+++ leetcode/242. Valid Anagram/index.js
@@ -24,3 +24,23 @@ var isAnagram = function(s, t) {
   }
   return true;
 };
+
+//  method2
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length != t.length) return false;
+  return (
+    s
+      .split("")
+      .sort()
+      .join("") ==
+    t
+      .split("")
+      .sort()
+      .join("")
+  );
+};
