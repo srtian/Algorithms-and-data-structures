0035036e0dc748595a1778f5ad8f76786cf51223
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-21 17:16:55 +0800

    add no.242 valid anagram

---
+++ leetcode/242. Valid Anagram/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  const sSize = s.length;
+  const tSize = t.length;
+  if (sSize !== tSize) return false;
+  let dict = {};
+  for (let i = 0; i < sSize; i++) {
+    if (dict[s[i]]) {
+      dict[s[i]] = dict[s[i]] + 1;
+    } else {
+      dict[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < tSize; i++) {
+    if (dict[t[i]]) {
+      dict[t[i]] = dict[t[i]] - 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
