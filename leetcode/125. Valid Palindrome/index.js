3d1c10926f05fac2095e0d359683c80b0590c041
ruotian.shen <ruotian.shen@horizon.ai>
2020-03-21 17:36:44 +0800

    add the second method for no.125 vaild palindrome

--- leetcode/125. Valid Palindrome/index.js
+++ leetcode/125. Valid Palindrome/index.js
@@ -9,3 +9,15 @@ var isPalindrome = function(s) {
     .split("");
   return arr.join("") === arr.reverse().join("");
 };
+
+// method 2
+var isPalindrome = function(s) {
+  s = s.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  const size = s.length;
+  for (let i = 0; i < size / 2; i++) {
+    if (s[i] !== s[size - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
