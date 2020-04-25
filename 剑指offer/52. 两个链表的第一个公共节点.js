7faee328d23bcb068adeb82f14a6e219058eb9ac
ruotian.shen <ruotian.shen@horizon.ai>
2020-04-25 16:34:32 +0800

    add(leetcode): 52.两个链表的第一个公共节点

---
+++ 剑指offer/52. 两个链表的第一个公共节点.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  let nodeA = headA;
+  let nodeB = headB;
+  while (nodeA || nodeB) {
+    if (nodeA === nodeB) return nodeA;
+    nodeA = nodeA === null ? headB : nodeA.next;
+    nodeB = nodeB === null ? headA : nodeB.next;
+  }
+  return null;
+};
