3b1989b819c7fe42a92972f52b0f1932f06355b8
ruotian.shen <ruotian.shen@horizon.ai>
2020-05-02 14:33:56 +0800

    add no.21 合并两个有序链表——迭代法

--- leetcode/21.Merge Two Sorted Lists/index.js
+++ leetcode/21.Merge Two Sorted Lists/index.js
@@ -10,6 +10,7 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+// 递归法，将每一步的插入分解出来，只关心每一个节点的顺序
 function mergeTwoList(l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
@@ -21,3 +22,35 @@ function mergeTwoList(l1, l2) {
     return l1;
   }
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+// 迭代法，因为是顺序的，所以可以先将其全部对比一遍进行排序，
+// 如果多出来的，就放在链表后面即可
+var mergeTwoLists = function(l1, l2) {
+  let nodeList = new ListNode();
+  let cur = nodeList;
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      cur.next = l1;
+      l1 = l1.next;
+    } else {
+      cur.next = l2;
+      l2 = l2.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = l1 !== null ? l1 : l2;
+  return nodeList.next;
+};
