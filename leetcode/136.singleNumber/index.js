a8662c9eee7b84247a4b0733b4969ca4dbd60c86
srtian <shenruotian@outlook.com>
2020-05-14 23:10:28 +0800

    refactor: the 136

--- leetcode/136.singleNumber/index.js
+++ leetcode/136.singleNumber/index.js
@@ -2,11 +2,16 @@ var singleNumber = function(nums) {
   const size = nums.length;
   let dict = {};
   for (let i = 0; i < size; i++) {
-    if (dict[nums[i]]) {
+    if (dict[nums[i]] !== undefined) {
       delete dict[nums[i]];
     } else {
       dict[nums[i]] = 1;
     }
   }
   return Object.keys(dict)[0];
+};
+
+
+var singleNumber = function(nums) {
+  return nums.reduce((a, b) => a ^ b)
 };
--- leetcode/189. 旋转数组/index.js
+++ leetcode/189. 旋转数组/index.js
@@ -5,15 +5,13 @@
  */
 // 方法一，直接使用API去截取
 var rotate = function(nums, k) {
-  return nums.unshift(...nums.splice(nums.length - k, k));
+  return nums.unshift(...nums.splice(nums.length - k));
 };
 
 // 方法二，模仿队列
 var rotate = function(nums, k) {
-  if (!nums) return [];
-  while (k) {
-    var flag = nums.pop();
-    nums.unshift(flag);
+  while (k > 0) {
+    nums.unshift(nums.pop());
     k--;
   }
   return nums;
