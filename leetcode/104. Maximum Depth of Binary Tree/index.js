0c8c539616bed86a99eb1b3de46ebeef06fd6b0d
Srtian <shenruotian@kuaishou.com>
2021-04-23 16:00:03 +0800

    feat(LeetCode): 104最大深度BFS

--- leetcode/104. Maximum Depth of Binary Tree/index.js
+++ leetcode/104. Maximum Depth of Binary Tree/index.js
@@ -15,3 +15,39 @@ var maxDepth = function(root) {
   const right = maxDepth(root.right);
   return Math.max(left, right) + 1;
 };
+
+// BFS
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if(!root) return 0
+  let stack = [root]
+  let depth = 1
+  while(stack.length) {
+      let curLen = stack.length
+      for(let i = 0; i < curLen; i++) {
+          let cur = stack.shift()
+          if(cur.right) {
+              stack.push(cur.right)
+          }
+          if(cur.left) {
+              stack.push(cur.left)
+          }
+      }
+      if(stack.length) {
+          depth++
+      }
+  }
+  return depth
+};
