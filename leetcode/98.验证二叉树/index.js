9ee10373472a90e5a3ea67b721f4d3171622182a
Srtian <shenruotian@kuaishou.com>
2021-06-02 09:39:48 +0800

    feat: lc98 中序遍历解法

--- leetcode/98.验证二叉树/index.js
+++ leetcode/98.验证二叉树/index.js
@@ -19,3 +19,25 @@ var helper = (root, min , max) => {
     if(root.val>= max || root.val <= min) return false
     return helper(root.left, min, root.val) && helper(root.right, root.val, max)
 }
+
+
+// 中序遍历
+
+var isValidBST = function(root) {
+    let stack = [];
+    let inorder = -Infinity;
+
+    while (stack.length || root !== null) {
+        while (root !== null) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        if (root.val <= inorder) {
+            return false;
+        }
+        inorder = root.val;
+        root = root.right;
+    }
+    return true;
+}
