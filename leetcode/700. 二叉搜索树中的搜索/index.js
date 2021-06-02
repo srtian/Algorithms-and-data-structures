e2d2bcb79d0039b41c7da0612bdf5e84ef78f50b
Srtian <shenruotian@kuaishou.com>
2021-06-02 21:56:54 +0800

    feat: 解决700二叉搜索树中的搜索(迭代法)

--- leetcode/700. 二叉搜索树中的搜索/index.js
+++ leetcode/700. 二叉搜索树中的搜索/index.js
@@ -1,6 +1,15 @@
+// 递归法
 var searchBST = function(root, val) {
     if(!root || root.val === val) {
         return root
     }    
     return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val)
+};
+
+// 迭代法
+var searchBST = function(root, val) {
+    while(root && root.val !== val) {
+        root = root.val > val ? root.left : root.right
+    }
+    return root
 };
